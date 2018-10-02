87a3e9c3b53370622e6eb733b49a9704f10252d5
Abdul Kanjo <Abdul.kanjo@gmail.com>
2018-10-01 23:47:29 -0500

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -8,14 +8,47 @@ class App extends Component {
     super(props);
     this.state = {
       data: [],
-      loading: true
+      loading: true,
+      first: true,
+      sec: true,
+      third: true,
+      four: true
     };
   }
 
   componentDidMount() {
-    axios
+    this.mainFunc();
+  }
+
+  async mainFunc() {
+    await axios
       .get("https://swapi.co/api/people/1/")
-      .then(res => this.setState({ data: res.data, loading: false }))
+      .then(res => this.setState({ data: res.data }))
+      .then(
+        setTimeout(() => {
+          this.setState({ loading: false });
+        }, 3000)
+      )
+      .then(
+        setTimeout(() => {
+          this.setState({ first: false });
+        }, 6000)
+      )
+      .then(
+        setTimeout(() => {
+          this.setState({ sec: false });
+        }, 9000)
+      )
+      .then(
+        setTimeout(() => {
+          this.setState({ third: false });
+        }, 12000)
+      )
+      .then(
+        setTimeout(() => {
+          this.setState({ four: false });
+        }, 15000)
+      )
       .catch(err => console.log(err));
   }
 
@@ -23,14 +56,38 @@ class App extends Component {
     return this.state.loading ? (
       <div className="App">
         <div className="App-header">
-          <div>Please wait</div>
+          <div>Please Wait</div>
+        </div>
+      </div>
+    ) : this.state.first ? (
+      <div className="App">
+        <div className="App-header">
+          <div>Keep waiting...</div>
+        </div>
+      </div>
+    ) : this.state.sec ? (
+      <div className="App">
+        <div className="App-header">
+          <div>Keep waiting more...</div>
+        </div>
+      </div>
+    ) : this.state.third ? (
+      <div className="App">
+        <div className="App-header">
+          <div>Almost there!</div>
+        </div>
+      </div>
+    ) : this.state.four ? (
+      <div className="App">
+        <div className="App-header">
+          <div>Hahahaha this is funny</div>
         </div>
       </div>
     ) : (
       <div className="App">
         <div className="App-header">
-          <div>{this.state.data.name}</div>
-          <div>{this.state.data.eye_color}</div>
+          <div>Name: {this.state.data.name}</div>
+          <div>Eye color: {this.state.data.eye_color}</div>
         </div>
       </div>
     );
